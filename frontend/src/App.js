1da3c8b0f5d3a017b61a2e757405d2f1743a71f0
Renée Cruz <reneebasscruz@gmail.com>
2019-10-30 17:13:42 -0400

    sidebar, fetch, render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,40 @@
 import React, { Component, Fragment } from "react";
 import "./Stylesheets/style.scss";
+import ShowPanel from './Components/ShowPanel';
+import Sidebar from './Components/Sidebar';
+import Topbar from './Components/Topbar';
+
 
 export default class App extends Component {
+
+  state = {
+    people: [],
+    person: {}
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/people")
+    .then(res => res.json())
+    .then(peopleObj => {
+      this.setState({
+        people: peopleObj
+      })
+    })
+  }
+
+  handleClick = () => {
+    console.log("clicked bitch!!")
+  }
+
   render() {
-    return <Fragment>render your components here</Fragment>;
+    // console.log(this.state.people)
+    return <Fragment>
+      <Topbar />
+      <Sidebar
+        people={this.state.people}
+        handleClick={this.handleClick}
+      />
+      <ShowPanel />
+    </Fragment>;
   }
 }
--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Sidebar = () => {
-  return <ul className="sidebar-ul"></ul>;
+const Sidebar = (props) => {
+
+  return <ul className="sidebar-ul">
+    {props.people.map((person => {
+      return <li
+        className="sidebar-li"
+        onClick={props.handleClick}
+        key={person.id}>
+        {person.name}
+      </li>
+    }))}
+  </ul>;
 };
 
 export default Sidebar;
